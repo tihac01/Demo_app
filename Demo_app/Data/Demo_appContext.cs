5e49a26ee35c42a7c58eebb76d887dc30b9e67ea
TFernezir <tfernezir@conet.hr>
2026-01-27 12:52:16 +0100

    Add project files.

---
+++ Demo_app/Data/Demo_appContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Demo_app.Model;
+
+namespace Demo_app.Data
+{
+    public class Demo_appContext : DbContext
+    {
+        public Demo_appContext (DbContextOptions<Demo_appContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Demo_app.Model.Student> Student { get; set; } = default!;
+    }
+}
